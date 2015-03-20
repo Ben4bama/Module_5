cf27f563275e40aa91a3c24513899874ab29b4ea
Ben Crenshaw <ben4bama@gmail.com>
2015-03-19 23:27:10 -0500

    Added section 3

--- Module 5/js/Assignment5.js
+++ Module 5/js/Assignment5.js
@@ -106,3 +106,46 @@ function GenerateOutput_2(result)
     displaytext += "</table>";
     document.getElementById("orderhistorydisplay").innerHTML = displaytext;
 }
+
+// SECTION THREE
+
+function GetOrders()
+{
+    var objRequest = new XMLHttpRequest();  //Create AJAX request object
+    //Create URL and Query string
+    var url = "http://bus-pluto.ad.uab.edu/jsonwebservice/service1.svc/getOrdersForCustomer/customerID";
+    url += document.getElementById("custid").value;
+    //Checks that the object has returned data
+    objRequest.onreadystatechange = function()
+    {
+        if (objRequest.readyState == 4 && objRequest.status == 200)
+        {
+            var output = JSON.parse(objRequest.responseText);
+            GenerateOutput3(output);
+        }
+    }
+            //Initiate the server request
+            objRequest.open("GET", url, true);
+            objRequest.send();
+}
+function GenerateOutput3(result)
+{   var count = 0;
+    var displaytext = "";
+    displaytext += "<table>";
+    //Loop to extract data from the response object
+    for (count = 0; count < result.GetOrdersForCustomerResult.length; count++)
+    {
+        
+        displaytext += "<tr>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].OrderDate + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].OrderID + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].ShipAddress + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].ShipCity + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].ShipName + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].ShipPostcode + "</td>";
+            displaytext += "<td>" + result.GetOrdersForCustomerResult[count].ShippedDate + "</td>";
+        displaytext += "</tr>";
+    }
+    displaytext += "</table>";
+    document.getElementById("orderdisplay").innerHTML = displaytext;
+}
